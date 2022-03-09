ec8e7abef48675d8294c67b0c36aa126281388a3
Maxim <swiftdevfromsiberia@gmail.com>
2022-03-09 06:02:07 +0500

    added: api key, urlsession

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Makarov_Maxim on 09.03.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "f99b81b550d0b6ae86236444fd35bbe7"
+
