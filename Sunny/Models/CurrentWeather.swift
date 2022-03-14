ba59ed9f755faad7e97187db4df7abcf64d76993
Maxim <swiftdevfromsiberia@gmail.com>
2022-03-14 21:35:42 +0500

    animating view with clousures

--- Sunny/Models/CurrentWeather.swift
+++ Sunny/Models/CurrentWeather.swift
@@ -2,29 +2,43 @@
 //  CurrentWeather.swift
 //  Sunny
 //
-//  Created by Makarov_Maxim on 11.03.2022.
-//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//  Created by Ivan Akulov on 05/03/2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
 //
 
 import Foundation
 
 struct CurrentWeather {
     let cityName: String
+    
     let temperature: Double
     var temperatureString: String {
-        return "\(temperature.rounded())"
+        return String(format: "%.0f", temperature)
     }
+    
     let feelsLikeTemperature: Double
-    var feelsLiketemperatureString: String {
-        return "\(feelsLikeTemperature.rounded())"
+    var feelsLikeTemperatureString: String {
+        return String(format: "%.0f", feelsLikeTemperature)
     }
+    
     let conditionCode: Int
+    var systemIconNameString: String {
+        switch conditionCode {
+        case 200...232: return "cloud.bolt.rain.fill"
+        case 300...321: return "cloud.drizzle.fill"
+        case 500...531: return "cloud.rain.fill"
+        case 600...622: return "cloud.snow.fill"
+        case 700...781: return "smoke.fill"
+        case 800: return "sun.min.fill"
+        case 801...804: return "cloiud.fill"
+        default: return "nosign"
+        }
+    }
     
     init?(currentWeatherData: CurrentWeatherData) {
         cityName = currentWeatherData.name
         temperature = currentWeatherData.main.temp
         feelsLikeTemperature = currentWeatherData.main.feelsLike
         conditionCode = currentWeatherData.weather.first!.id
-        
     }
 }
